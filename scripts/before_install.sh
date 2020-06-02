634e71bb982ba05f0ad912b2a7eaa23c8fccfe6e
Ubuntu <ubuntu@ip-172-31-54-73.eu-west-1.compute.internal>
2020-06-02 01:02:22 +0000

    Removing the after_install hook

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,4 +4,5 @@ apt-get install zip
 
 mkdir -p /opt/webinar-site
 now=$(date +"%y-%m-%d_%H-%M-%S")
-zip "/opt/webinar-site/backup-${now}" /var/www/html/*
+zip -r "/opt/webinar-site/backup-${now}" /var/www/html
+rm -rf /var/www/html/*
